6fd6e24c40d4907f86e30c0358c8170f6fa95c7f
DanielIliev <Daniel.liev.work@gmail.com>
2015-01-25 01:42:34 +0200

    Send email and add as viewer

--- Diploma.js
+++ Diploma.js
@@ -1,10 +1,7 @@
-/**
- * Retrieves all the rows in the active spreadsheet that contain data and logs the
- * values for each row.
- * For more information on using the Spreadsheet API, see
- * https://developers.google.com/apps-script/service_spreadsheet
- */
+// Scans sheet for errors
+
 function scan_err() {
+  
   var sheet = SpreadsheetApp.getActiveSheet();
   var rows = sheet.getDataRange();
   var numRows = rows.getNumRows();
@@ -14,35 +11,51 @@ function scan_err() {
     var row = values[i];
     Logger.log(row);
   }
+  
 };
 
+
+// Fix errors in the schedule
+
 function fix_err() {
+
   Logger.log("Fix Errors");
+
 }
 
+
+// Send emails about the schedule to teachers
+
 function exporter() {
-  var sheet = SpreadsheetApp.getActiveSheet();
-  var rows = sheet.getDataRange();
-  var numRows = rows.getNumRows();
-  var values = rows.getValues();
-  var email_addresses = [];
-  var number_of_emails = Browser.inputBox("Number of emails", Browser.Buttons.OK_CANCEL);
-  var number_of_emails = parseInt(number_of_emails);
-  for (var i = 0; i <= numRows - 1; i++) {
-    var message = values;       // Data from spreadsheet
-    var subject = "Sending emails from a Spreadsheet";
-    MailApp.sendEmail(emailAddress, subject, message);
+
+  state_check_email = false;
+  while (state_check_email !== true ) {
+    if (state_check_email === false) {
+      var email = Browser.inputBox("Input a valid email of teacher", Browser.Buttons.OK_CANCEL);
+      var email_validity_pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      var validity_result = email_validity_pattern.test(email);
+      if (!validity_result) {
+        Browser.msgBox("Invalid email, try again");
+      } else {
+        var url_fetch = SpreadsheetApp.getActiveSpreadsheet();
+        var url = url_fetch.getUrl();
+        var add_viewer_address = SpreadsheetApp.getActiveSpreadsheet();
+        add_viewer_address.addViewer(email);
+        var subject = "Welcome";
+        MailApp.sendEmail(email, subject, "Welcome --> This is the url to the schedule " + url);
+        Browser.msgBox("Success");
+        var state_check_email = true;
+      }
+    }
   }
+
 }
-/**
- * Adds a custom menu to the active spreadsheet, containing a single menu item
- * for invoking the readRows() function specified above.
- * The onOpen() function, when defined, is automatically invoked whenever the
- * spreadsheet is opened.
- * For more information on using the Spreadsheet API, see
- * https://developers.google.com/apps-script/service_spreadsheet
- */
+
+
+// Menu in spreadsheet implementation
+
 function application_start() {
+
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var entries = [{
     name : "Scan for errors",
@@ -57,4 +70,5 @@ function application_start() {
     functionName : "exporter",
   }];
   spreadsheet.addMenu("Schedule_fixer", entries);
+
 };
