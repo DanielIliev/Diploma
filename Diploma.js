fa3b088b20945f15356179407259fc1cc26ade9c
DanielIliev <Daniel.liev.work@gmail.com>
2015-03-24 11:51:27 +0200

    Update Diploma.js

--- Diploma.js
+++ Diploma.js
@@ -3,8 +3,10 @@ function uiMenu() {
   .createAddonMenu()
   .addItem('Разписание учител', 'teachers')
   .addSeparator()
-  .addItem('Маркирай дубликати', 'mark_duplicates')
-  .addToUi();
+  .addItem('Маркирай дублиращи', 'mark_duplicates')
+  .addSeparator()
+  .addItem('График на учебни кабинети', 'cabinets')
+  .addToUi()
 }
 
 function mark_duplicates() {
@@ -18,19 +20,19 @@ function mark_duplicates() {
       for (var col = 0; col < cols; col++) {
         switch(data[row][col]) {
           case "VIII клас":
-            class_VIII(rows,cols,data,range);
+            class_VIII(rows,cols,data,range)
             break;
           case "IX клас":
-            class_IX(rows,cols,data,range);
+            class_IX(rows,cols,data,range)
             break;
           case "X клас":
-            class_X_XI_XII(rows,cols,data,range);
+            class_X(rows,cols,data,range)
             break;
-          case "XI клас":
-            class_X_XI_XII(rows,cols,data,range);
+          case 'XI клас':
+            class_XI(rows,cols,data,range)
             break;
           case "XII клас":
-            class_X_XI_XII(rows,cols,data,range);
+            class_XII(rows,cols,data,range)
             break;
         }
       }
@@ -43,22 +45,22 @@ function class_VIII(rows,cols,data,range) {
     for (var col = 0; col < cols; col++) {
       if (data[row][col].length > 0 && data[row][col].length <= 3) {
         switch(data[row][col]) {
-          case data[row][col+5]:
+          case data[row][col+4]:
             var cell1 = range.getCell(row+1,col);
             cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+5);
+            var cell2 = range.getCell(row+1,col+4);
             cell2.setBackground("red");
             break;
-          case data[row][col+10]:
+          case data[row][col+8]:
             var cell1 = range.getCell(row+1,col);
             cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+10);
+            var cell2 = range.getCell(row+1,col+8);
             cell2.setBackground("red");
             break;
-          case data[row][col+15]:
+          case data[row][col+12]:
             var cell1 = range.getCell(row+1,col);
             cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+15);
+            var cell2 = range.getCell(row+1,col+12);
             cell2.setBackground("red");
         }
       }
@@ -67,27 +69,29 @@ function class_VIII(rows,cols,data,range) {
 }
 
 function class_IX(rows,cols,data,range) {
-  for (var row = 0; row < rows; row++) {
-    for (var col = 0; col < cols; col++) {
+  for (var row = 1; row < rows; row++) {
+    for (var col = 1; col < cols; col++) {
       if (data[row][col].length > 0 && data[row][col].length <= 3) {
         switch(data[row][col]) {
-          case data[row][col+6]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+4);
+          case data[row][col+5]:
+            var cell1 = range.getCell(row+1,col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1,col+5)
             cell2.setBackground("red");
+            cell2 = range.getCell(row+1,col+4)
+            cell2.setBackground("red")
             break;
-          case data[row][col+12]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+11);
-            cell2.setBackground("red");
+          case data[row][col+10]:
+            var cell1 = range.getCell(row+1,col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1,col+10)
+            cell2.setBackground("red")
             break;
-          case data[row][col+18]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+17);
-            cell2.setBackground("red");
+          case data[row][col+15]:
+            var cell1 = range.getCell(row+1,col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1,col+15)
+            cell2.setBackground("red")
             break;
         }
       }
@@ -95,185 +99,89 @@ function class_IX(rows,cols,data,range) {
   }
 }
 
-function class_X_XI_XII(rows,cols,data,range) {
+function class_X(rows,cols,data,range) {
   for (var row = 0; row < rows; row++) {
     for (var col = 0; col < cols; col++) {
       if (data[row][col].length > 0 && data[row][col].length <= 3) {
         switch(data[row][col]) {
-          case data[row][col+6]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            cell1 = range.getCell(row+1,col);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+6);
-            cell2.setBackground("red");
-            cell2 = range.getCell(row+1,col+5);
-            cell2.setBackground("red");
-          case data[row][col+11]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            cell1 = range.getCell(row+1,col);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+11);
-            cell2.setBackground("red");
-            cell2 = range.getCell(row+1,col+10);
-            cell2.setBackground("red");
+          case data[row][col+5]:
+            var cell1 = range.getCell(row+1, col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1, col+5)
+            cell2.setBackground("red")
             break;
-          case data[row][col+16]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            cell1 = range.getCell(row+1,col);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+16);
-            cell2.setBackground("red");
-            cell2 = range.getCell(row+1,col+15);
-            cell2.setBackground("red");
+          case data[row][col+10]:
+            var cell1 = range.getCell(row+1, col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1, col+10)
+            cell2.setBackground("red")
             break;
-          case data[row][col+22]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            cell1 = range.getCell(row+1,col);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+22);
-            cell2.setBackground("red");
-            cell2 = range.getCell(row+1,col+21);
-            cell2.setBackground("red");
+          case data[row][col+15]:
+            var cell1 = range.getCell(row+1, col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1, col+15)
+            cell2.setBackground("red")
             break;
-          case data[row][col+27]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            cell1 = range.getCell(row+1,col);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+27);
-            cell2.setBackground("red");
-            cell2 = range.getCell(row+1,col+26);
-            cell2.setBackground("red");
+          case data[row][col+20]:
+            var cell1 = range.getCell(row+1, col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1, col+20)
+            cell2.setBackground("red")
             break;
-          case data[row][col+33]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            cell1 = range.getCell(row+1,col);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+33);
-            cell2.setBackground("red");
-            cell2 = range.getCell(row+1,col+32);
-            cell2.setBackground("red");
+          case data[row][col+25]:
+            var cell1 = range.getCell(row+1, col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1, col+25)
+            cell2.setBackground("red")
             break;
-          case data[row][col+38]:
-            var cell1 = range.getCell(row+1,col-1);
-            cell1.setBackground("red");
-            cell1 = range.getCell(row+1,col);
-            cell1.setBackground("red");
-            var cell2 = range.getCell(row+1,col+38);
-            cell2.setBackground("red");
-            cell2 = range.getCell(row+1,col+37);
-            cell2.setBackground("red");
+          case data[row][col+30]:
+            var cell1 = range.getCell(row+1, col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1, col+30)
+            cell2.setBackground("red")
+            break;
+          case data[row][col+35]:
+            var cell1 = range.getCell(row+1, col)
+            cell1.setBackground("red")
+            var cell2 = range.getCell(row+1, col+35)
+            cell2.setBackground("red")
             break;
         }
       }
+    }
+  }
+}
+
+function class_XI(rows,cols,data,range) {
+  for (var row = 0; row < rows; row++) {
+    for (var col = 0; col < cols; col++) {
+      Logger.log(data[row][col])
+    }
+  }
+}
+
+function class_XII(rows,cols,data,range) {
+  for (var row = 0; row < rows; row++) {
+    for (var col = 0; col < cols; col++) {
+      Logger.log(data[row][col])
     }
   }
 }
 
 function teachers() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets();
-  var initials = Browser.inputBox("Въведи инициали на преподавател");
-  if (initials != "cancel") {
-    for (var i = 0; i < sheet.length; i++) {
-      var rows = sheet[i].getDataRange().getNumRows();
-      var cols = sheet[i].getDataRange().getNumColumns();
-      var data = sheet[i].getDataRange().getValues();
-      for (var row = 0; row < rows; row++) {
-        for (var col = 0; col < cols; col++) {
-          var inits_check = search_init_sheet(rows,cols,data,initials);
-          if (inits_check) {
-            switch(data[row][col]) {
-              case "VIII клас":
-                var new_sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("График на " + initials + " за VIII клас", sheet.length + 1)
-                var range = new_sheet.getRange(1,1,data.length,data[0].length);
-                range.setValues(data);
-                get(range, data, initials);
-                break;
-              case "IX клас":
-                var new_sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("График на " + initials + " за IX клас", sheet.length + 1)
-                var range = new_sheet.getRange(1,1,data.length,data[0].length);
-                range.setValues(data);
-                get(range, data, initials);
-                break;
-              case "X клас":
-                var new_sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("График на " + initials + " X клас", sheet.length + 1)
-                var range = new_sheet.getRange(1,1,data.length,data[0].length);
-                range.setValues(data);
-                get(range, data, initials);
-                break;
-              case "XI клас":
-                var new_sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("График на " + initials + " за XI клас", sheet.length + 1)
-                var range = new_sheet.getRange(1,1,data.length,data[0].length);
-                range.setValues(data);
-                get(range, data, initials);
-                break;
-              case "XII клас":
-                var new_sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("График на " + initials + " за XII клас", sheet.length + 1)
-                var range = new_sheet.getRange(1,1,data.length,data[0].length);
-                range.setValues(data);
-                get(range, data, initials);
-                break;
-            }
-          }
-        }
-      }
-    }
-  }
+  Browser.msgBox('Тази функционалност е в процес на разработка')
 }
 
-function get(range, data, inits) {
-  var rows = range.getNumRows();
-  var cols = range.getNumColumns();
-  for (var row = 0; row < rows; row++) {
-    var time = search_time(cols,data[row]);
-    if (time) {
-      var init_check = search_inits(cols, data[row], inits);
-      if (!init_check) {
-        for (var col = 0; col < cols; col++) {
-          var cell = range.getCell(row+1,col+1);
-          cell.setBackground("red");
-        }
-      }
-    }
-  }
-}
-
-function search_time(cols,data_row) {
-  var timestamp = /[0-9]+[:]{1}[0-9]+/;
-  var found = false;
-  for (var col = 0; col < cols; col++) {
-    if (data_row[col] != "") {
-      if (data_row[col].toString().search(timestamp) != -1) {
-        found = true;
-      }
-    }
-  }
-  return found;
-}
-
-function search_init_sheet(rows,cols,data,inits) {
-  var found = false;
-  for (var row = 0; row < rows; row++) {
-    for (var col = 0; col < cols; col++) {
-      if (data[row][col] == inits) {
-        found = true;
-      }
-    }
-  }
-  return found;
+function cabinets() {
+  Browser.msgBox('Тази функционалност е в процес на разработка')
 }
 
 function search_inits(cols,data_row,inits) {
-  var found = false;
+  var found = false
   for (var col = 0; col < cols; col++) {
     if (data_row[col] == inits) {
-      found = true;
+      found = true
     }
   }
-  return found;
+  return found
 }
